0b0bb717fcfa44b534f068d4cad92da799b95372
ma1phur <ol25863@gmail.com>
2019-09-19 10:17:35 +0900

    translations: added translated files for korean users

---
+++ strings/ko.c
@@ -0,0 +1,21 @@
+.Config_NotReadable =
+    "TinfoilMod 설정을 읽을 수 없습니다.\n"
+    "sdmc:/switch/tinfoil/mod_config.ini에 파일을 확인해주십시오.";
+.Config_WrongDeviceTag =
+    "TinfoilMod 설정 경고: Device!Tag 값이 이 기기와 일치하지 않습니다.\n"
+	"이 기기의 값은 Console 탭에서 찾을 수 있습니다.";
+.Telemetry_CannotDisableTinfoilIo =
+    "TinfoilMod 설정 경고: Device!Tag 값이 이 기기와 일치하지 않습니다.\n"
+    "그러므로, Tinfoil.io으로 발신되는 정보를 비활성화할 수 없습니다.\n"
+	"이 기기의 값은 Console 탭에서 찾을 수 있습니다.";
+.UI_CannotRemoveAboutTab =
+    "TinfoilMod 설정 경고: Device!Tag 값이 이 기기와 일치하지 않습니다.\n"
+    "그러므로, About 탭을 비활성화할 수 없습니다.\n"
+	"이 기기의 값은 Console 탭에서 찾을 수 있습니다.";
+.Misc_LocalFileChecksDisabled =
+    "TinfoilMod 설정 경고: Device!Tag 값이 이 기기와 일치하지 않습니다.\n"
+    "그러므로, 파일 검증 우회를 비활성화 할 수 없습니다.\n"
+    "틴포일이 제대로 작동하기위해서, hbl 2.1 버전을 사용해주시길 바랍니다.\n"
+	"이 기기의 Device!Tag는 %.8s 입니다.";
+.Tab_ModConfig =
+    "모드 설정";
