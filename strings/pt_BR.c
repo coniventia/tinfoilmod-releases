9d8c771c8675504748c5d0d60a473430be494a7b
coniventia <54706216+coniventia@users.noreply.github.com>
2019-09-23 20:30:50 +0200

    translations: added pt-BR translations from dbz

---
+++ strings/pt_BR.c
@@ -0,0 +1,21 @@
+.Config_NotReadable =
+    "A config do TinfoilMod não pôde ser lida.\n"
+    "Por favor cheque o arquivo sdmc:/switch/tinfoil/mod_config.ini.";
+.Config_WrongDeviceTag =
+    "Aviso do TinfoilMod: Device!Tag não corresponde a este dispositivo.\n"
+    "O valor para o seu dispositivo pode ser encontrado na aba Console";
+.Telemetry_CannotDisableTinfoilIo =
+    "Aviso do TinfoilMod: Device!Tag não corresponde a este dispositivo.\n"
+    "Por isso a telemetria para Tinfoil.io não pôde ser desabilitada.\n"
+    "O valor para o seu dispositivo pode ser encontrado na aba Console";
+.UI_CannotRemoveAboutTab =
+    "Aviso do TinfoilMod: Device!Tag não corresponde a este dispositivo.\n"
+    "Por isso a aba Sobre não pôde ser desabilitada.\n"
+    "O valor para o seu dispositivo pode ser encontrado na aba Console";
+.Misc_LocalFileChecksDisabled =
+    "Aviso do TinfoilMod: Device!Tag não corresponde a este dispositivo.\n"
+    "Por isso a verificação de arquivos não pode ser habilitada.\n"
+    "Para o Tinfoil funcionar como o esperado, por favor use o hbl 2.1.\n"
+    "O valor do Device!Tag para o seu dispositivo é: %.8s.";
+.Tab_ModConfig =
+    "Configuração do Mod";
